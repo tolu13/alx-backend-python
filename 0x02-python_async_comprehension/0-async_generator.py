75c0d8638f40eb563e4ccb732dc7c4d7d07ed567
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-05-07 18:11:55 +0000

    k

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,9 +9,9 @@ Use the random module.
 """
 import random
 import asyncio
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator [float, None, None]:
     """ An async generator that takes no arguement """
     for _ in range(10):
         await asyncio.sleep(1)
